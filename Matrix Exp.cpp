25ff02d885fabc3739f448a7835353e08f86eff8
Rupanshu Yadav <58172798+zyrch@users.noreply.github.com>
2020-06-29 20:29:15 +0530

    Create Matrix Exp.cpp

---
+++ Matrix Exp.cpp
@@ -0,0 +1,34 @@
+ll siz;
+struct Matrix{
+    vector<vector<ll>> mat;
+    int nax;
+    Matrix (int siz) {
+	nax = siz;
+	mat.resize(nax, vl(nax));
+    }
+    Matrix operator*(Matrix other){
+        Matrix ans(other.nax);
+        for(int i = 0; i < nax; i++){
+            for(int j = 0; j < nax; j++){
+                for(int k = 0; k < nax; k++) {
+                    ans.mat[i][j] = (ans.mat[i][j]+(mat[i][k]*other.mat[k][j])%mod)%mod;
+		}
+            }
+        }
+        return ans;
+    }
+};
+
+Matrix fast_exp(Matrix base,ll power){
+    Matrix x=base,ans(siz);
+    fo(i, n) {
+	ans.mat[i][i] = 1;
+    }
+    ll cnt = power;
+    while(cnt){
+        if (cnt&1) ans=(ans*x);
+        x=(x*x);
+        cnt>>=1;
+    }
+    return ans;
+}
