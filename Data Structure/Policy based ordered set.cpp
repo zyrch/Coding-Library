75513839bb2b8df829a234475ece5ed673ed9edc
Rupanshu Yadav <58172798+zyrch@users.noreply.github.com>
2020-07-10 02:17:41 +0530

    Create Policy based ordered set.cpp

---
+++ Data Structure/Policy based ordered set.cpp
@@ -0,0 +1,6 @@
+// template ends here
+
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+using namespace __gnu_pbds;
+template<class T> using oset = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
