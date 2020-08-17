6a32579918f3651e47204456df2ccc5fffbbf412
Rupanshu Yadav <58172798+zyrch@users.noreply.github.com>
2020-08-17 19:23:36 +0530

    Create Segtree point update.cpp

---
+++ Data Structure/Segtree point update.cpp
@@ -0,0 +1,58 @@
+// template ends here
+struct Segtree {
+    int siz;
+    vector<ll> tree;
+    vector<ll> lo;
+    vector<ll> hi;
+    Segtree(int n, vector<ll> arr) {
+	siz = 4 * n;
+	tree.resize(siz + 1);
+	lo.resize(siz + 1);
+	hi.resize(siz + 1);
+	build(1, n, arr, 1);
+    }
+    
+    void build(ll l, ll r, vector<ll> &arr, ll idx) {
+	
+	lo[idx] = l, hi[idx] = r;
+	if (l == r) {
+	    tree[idx] = arr[l - 1];
+	    return;
+	}
+	
+	ll mid = (l + r)/2;
+	build(l, mid, arr, 2 * idx);
+	build(mid + 1, r, arr, 2 * idx + 1);    
+	merge(idx);
+    }
+    
+    void update(ll idx, ll pos, ll val) {
+	if (hi[idx] < pos || lo[idx] > pos) {
+	    return;
+	}
+	if (lo[idx] == hi[idx]) {
+	    tree[idx] = val;
+	    return;
+	}
+	update(2 * idx, pos, val);
+	update(2 * idx + 1, pos, val);
+	merge(idx);
+    }
+    
+    ll query(ll idx, ll l, ll r) {
+	if (hi[idx] < l || lo[idx] > r) {
+	    return 0;
+	}
+	if (l <= lo[idx] && hi[idx] <= r) {
+	    return tree[idx];
+	}
+	
+	ll Left = query(2 * idx, l, r);
+	ll Right = query(2 * idx + 1, l, r);
+	return max(Left, Right);
+    }
+    
+    void merge(ll idx) {
+	tree[idx] = max(tree[2 * idx], tree[2 * idx + 1]);
+    }
+};
